c06d07e72d15b7cbf051ecf63b11dadaa506619f
mohammad <mbh3@njit.edu>
2024-04-25 03:09:33 -0400

    Milestone 2 files and work

---
+++ Project/Common/Constants.java
@@ -0,0 +1,7 @@
+package Project.Common;
+// mbh3
+//* 04/25/24
+public abstract class Constants {
+    public static final String LOBBY = "lobby";
+    public static final long DEFAULT_CLIENT_ID = -1;
+}
